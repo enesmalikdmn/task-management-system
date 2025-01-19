ca7460d3e7f9fe9438691a42dce2a6717b871145
enesmalikdmn <m.duman@digitastic.app>
2025-01-19 16:30:16 +0300

    feat: editing story point

--- components/TaskList/TaskRepresentation.tsx
+++ components/TaskList/TaskRepresentation.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Avatar from '@mui/material/Avatar';
-import { TextField } from '@mui/material';
+import { TextField, Menu } from '@mui/material';
 import { useState } from 'react';
 
 export default function TaskRepresentation({
@@ -19,10 +19,19 @@ export default function TaskRepresentation({
 }) {
   const [taskName, setTaskName] = useState(task.name);
   const [isEditing, setIsEditing] = useState(false);
+  const [anchorElStatus, setAnchorElStatus] = useState<HTMLElement | null>(null);
+  const [ statusList, setStatusList ] = useState(['open', 'in progress', 'in review', 'done']);
+  const [isEditingStoryPoint, setIsEditingStoryPoint] = useState(false);
+  const [taskStoryPoint, setTaskStoryPoint] = useState(task.storyPoint);
 
   const handleTaskName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTaskName(event.target.value);
     task.name = event.target.value;
+  };
+
+  const handleTaskStoryPoint = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskStoryPoint(Number(event.target.value));
+    task.storyPoint = Number(event.target.value);
   };
 
   const handleClick = () => {
@@ -32,6 +41,11 @@ export default function TaskRepresentation({
   const handleBlur = () => {
     setIsEditing(false);
   };
+
+  const handleOpenStatusMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElStatus(event.currentTarget);
+  };
+
 
   return (
     <div className="flex flex-col pl-8 sm:flex-row justify-between items-start sm:items-center p-4 border border-gray-300 rounded-lg bg-white overflow-hidden shadow-lg">
@@ -68,12 +82,61 @@ export default function TaskRepresentation({
       )}
       </div>
       <div className="flex gap-4 mt-2 sm:mt-0 items-center w-full sm:w-auto">
-        <div className="w-24 flex justify-center px-2 py-1 rounded text-sm font-bold bg-[#e9f2ff] text-[#0052cc] shrink-0">
+        <div onClick={(event) => handleOpenStatusMenu(event)} className="w-24 flex justify-center px-2 py-1 rounded text-sm font-bold bg-[#e9f2ff] text-[#0052cc] shrink-0">
           {task.workflowStatus}
         </div>
-        <div className="w-6 h-6 flex items-center justify-center px-2 py-1 rounded-full text-sm font-bold bg-green-100 text-green-800 shrink-0">
-          {task.storyPoint}
+        <Menu
+          anchorEl={anchorElStatus}
+          open={Boolean(anchorElStatus)}
+          onClose={() => setAnchorElStatus(null)}
+        > <div className="flex flex-col items-center gap-2 w-[8rem]">
+          {
+          statusList.map((status, index) => (
+            <div
+              key={index}
+              className="w-24 cursor-pointer flex justify-center px-2 py-1 rounded text-sm font-bold bg-[#e9f2ff] text-[#0052cc] shrink-0"
+              onClick={() => {
+                task.workflowStatus = status;
+                setAnchorElStatus(null);
+              }}
+            >
+              {status}
+            </div>
+          ))
+        }
         </div>
+
+        </Menu>
+        {isEditingStoryPoint ? (
+          <TextField
+            value={taskStoryPoint}
+            onChange={handleTaskStoryPoint}
+            onBlur={() => setIsEditingStoryPoint(false)}
+            size="small"
+            variant="standard"
+            autoComplete="off"
+            autoFocus
+            sx={{
+              width: '3rem',
+              '& .MuiInput-underline:before': {
+                borderBottom: 'none',
+              },
+              '& .MuiInput-underline:after': {
+                borderBottom: 'none',
+              },
+              '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+                borderBottom: 'none',
+              },
+            }}
+          />
+        ): (
+          <div
+            className="w-6 h-6 flex items-center justify-center px-2 py-1 rounded-full text-sm font-bold bg-green-100 text-green-800 shrink-0"
+            onClick={() => setIsEditingStoryPoint(true)}
+          >
+            {task.storyPoint}
+          </div>
+        )}
         <Avatar
           className="!w-8 !h-8 shrink-0"
           alt={task.assignedTo}
